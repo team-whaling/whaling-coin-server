3768fdeb4dcb8beab44f6208c2932b4c6ebeea3a
Hwanu Jung <sossont1@gmail.com>
2022-01-01 15:32:31 +0900

    Init app

--- whaling-coin/settings/base.py
+++ whaling-coin/settings/base.py
@@ -24,13 +24,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oh&^so^6ykdxl+&7t)#1a8qeu+v(b)ea1upe3-#tmpcl8^!v=h'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
