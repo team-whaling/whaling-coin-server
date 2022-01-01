3127f42555d305da6e334271574c15822692cab3
Hwanu Jung <sossont1@gmail.com>
2022-01-01 16:25:53 +0900

    [WHAL-35] Feat : 모델링

---
+++ coins/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class DateModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)    # save될 때마다 날짜 갱신
+
+class Coin(DateModel):
+    coin_code = models.CharField(max_length=10,primary_key=True)    # PK
+    coin_name = models.CharField(max_lenght=12,null=False)
+    coin_image = models.URLField(null=False)
+    cur_price = models.IntegerField(null=False)
+
