d651c2733f2a0b23bf0e03f09f1b6afe435de796
Hwanu Jung <sossont1@gmail.com>
2022-01-05 21:51:41 +0900

    [WHAL-33] Feat : 표현 하는 정보 수정

--- coins/serializers.py
+++ coins/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class CryptoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cryptocurrency
-        fields = ['coin_code', 'coin_name', 'coin_image', 'cur_price', 'trade_date', 'trade_time']
+        fields = ['coin_code', 'coin_name', 'coin_image', 'cur_price', 'trade_date', 'trade_time', 'updated_at']
