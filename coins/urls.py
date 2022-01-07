a8d20e0fe6df7ef2f4db1ce44340d7405812de9b
Hwanu Jung <sossont1@gmail.com>
2022-01-07 21:46:12 +0900

    [WHAL-35] Feat : View Query기능 추가

--- coins/urls.py
+++ coins/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path,include
-from rest_framework import routers
 from .views import CryptoView
 
 app_name = 'coins'
 
 urlpatterns = [
-    path(r'coins', CryptoView.as_view()),
+    path('coins/', CryptoView.as_view()).
 ]
