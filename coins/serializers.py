2376c6c28f9043c5f185e9581ea57d464a24ccf8
Hwanu Jung <sossont1@gmail.com>
2022-01-08 00:46:04 +0900

    [WHAL-35] 서버 오류 해결

--- coins/serializers.py
+++ coins/serializers.py
@@ -4,4 +4,5 @@ from rest_framework import serializers
 class CryptoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cryptocurrency
-        fields = ['coin_code', 'coin_name', 'coin_image', 'cur_price', 'trade_date', 'trade_time', 'updated_at']
+        fields = ['coin_code', 'coin_name', 'coin_image', 'cur_price', 'trade_date', 'trade_time',
+                  'updated_date','updated_time']
