60d6cdb7c76aedac43a18a04ea81b54b965c51f3
Hwanu Jung <sossont1@gmail.com>
2022-01-11 15:42:10 +0900

    [WHAL-35] Feat : URL Query 2개 이상 받을 수 있게 수정

--- coins/tests.py
+++ coins/tests.py
@@ -1,13 +1,17 @@
 from django.test import TestCase
 import pytz
 from datetime import datetime,timedelta
+from dateutil.relativedelta import relativedelta
 # Create your tests here.
 
 kst_time = datetime.now(pytz.timezone('Asia/Seoul')).replace(tzinfo=None,microsecond=0,second=0)
 utc_time = datetime.now(pytz.timezone('UTC')).replace(tzinfo=None,microsecond=0,second=0)
 time = kst_time-utc_time
 print(kst_time, utc_time)
-utc_time += timedelta(hours=9)
+kst_time += relativedelta(months=1)
+utc_time += relativedelta(months=1,hours=9)
+print(kst_time)
+timedelta()
 if kst_time == utc_time:
     print("t")
 else:
--- coins/views.py
+++ coins/views.py
@@ -8,7 +8,9 @@ class CryptoView(generics.ListAPIView):
     serializer_class = CryptoSerializer
     def get_queryset(self):
         quaryset = Cryptocurrency.objects.all()
-        coincode = self.request.query_params.get('coin_code')
+        coincode = self.request.query_params.getlist('coin_code',None)
+        if len(coincode) == 0:
+            coincode = None
         if coincode is not None:
-            quaryset = quaryset.filter(coin_code=coincode)
+            quaryset = quaryset.filter(coin_code__in=coincode)
         return quaryset
